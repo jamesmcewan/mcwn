977e9f01614dbd1b32a6702b9700d08badcaac1d
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-08-05 03:09:23 +0100

    move awat from scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 const yaml = require("js-yaml");
 const htmlmin = require("html-minifier");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addDataExtension("yml", contents => yaml.safeLoad(contents));
   eleventyConfig.addPassthroughCopy({ "src/static": "/" });
+  eleventyConfig.addFilter(
+    "cssmin",
+    code => new CleanCSS({}).minify(code).styles
+  );
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if (outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
