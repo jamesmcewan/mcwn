e23b9dc4dc568627effb90611d750532ee66482a
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2021-10-06 14:12:35 +0100

    regular updates (#79)
    Co-authored-by: James McEwan <jamesmcewan@safeguardglobal.com>

--- cypress/integration/home.js
+++
@@ -1,8 +0,0 @@
-describe("Home", () => {
-  it("should render the Homepage", () => {
-    cy.visit("/");
-    cy.eyesOpen({ appName: "mcwn", testname: "home" });
-    cy.eyesCheckWindow({ tag: "Homepage", target: "window", fully: true });
-    cy.eyesClose();
-  });
-});
