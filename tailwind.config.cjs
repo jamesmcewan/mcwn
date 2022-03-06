1eab2a68b5fcfee572d64f0040c00d701cabfa3c
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2022-03-06 18:36:23 +0000

    comic colours (#128)
    * comic colours
    
    * content
    
    * update deps
    
    Co-authored-by: James McEwan <jamesmcewan@safeguardglobal.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,17 @@ module.exports = {
   plugins: [require("@tailwindcss/typography")],
   theme: {
     fontFamily: {
-      display: ["Permanent Marker", "san-serif"],
-      body: ["Merriweather", "serif"],
+      display: ["Cherry Cream Soda", "serif"],
+      body: ["PT Sans", "sans-serif"],
+    },
+    colors: {
+      "y3-r2": "#FBC991",
+      "y-r-b": "#252920",
+      "y-r-b4": "#772C26",
+      "y2-r3-b2": "#D392AA",
+      "r2-b2": "#D8C7E3",
+      "r3-b3": "#AB8CC1",
+      "y4-r-b4": "#4C2B44",
     },
   },
 };
