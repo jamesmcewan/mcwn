e916dd1ef0420999e453f2d7940aea829f25cbb2
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2022-05-17 01:26:42 +0100

    font font font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,17 +6,10 @@ module.exports = {
   ],
   plugins: [require("@tailwindcss/typography")],
   theme: {
-    fontFamily: {
-      body: ["Cormorant Garamon", "serif"],
-    },
-    colors: {
-      "y3-r2": "#FBC991",
-      "y-r-b": "#252920",
-      "y-r-b4": "#772C26",
-      "y2-r3-b2": "#D392AA",
-      "r2-b2": "#D8C7E3",
-      "r3-b3": "#AB8CC1",
-      "y4-r-b4": "#4C2B44",
+    extend: {
+      fontFamily: {
+        body: ["Playfair Display", "serif"],
+      },
     },
   },
 };
