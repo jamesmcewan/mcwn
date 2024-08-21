eee11916b7a7a9103cb34be3ec3f2568e8080599
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-08-21 22:21:49 +0100

    sst

---
+++ sst-env.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    "mcwn-astro": {
+      "type": "sst.aws.Astro"
+      "url": string
+    }
+  }
+}
+export {}
