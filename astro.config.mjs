f7541c66c83a4374024220e5a4cdd8667ffbd60e
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-09-10 23:46:28 +0100

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import aws from "astro-sst";
 
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://jamesmcewan.com/",
   output: "static",
-  adapter: aws(),
 });
