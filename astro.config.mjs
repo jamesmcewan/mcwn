2f53c7086ca38d7aff8341b39eb8511a9fe258db
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-01-25 23:09:52 +0000

    update bun.lock for 1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
-
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://mightydinosaur.dev/",
   output: "static",
-  adapter: netlify(),
 });
-
