dacaf6b7796cab36b0b8680c35718dc72391191a
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-02-24 11:00:05 +0000

    youtube fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,5 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from 'markdown-it'
 import { santise } from 'src/helper/santise'
 const parser = new MarkdownIt()
@@ -14,7 +13,7 @@ async function getItems() {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      content: santise(parser.render(post.body)),
+      content: parser.render(post.body),
       link: `/posts/${post.slug}/`,
     }
   })
