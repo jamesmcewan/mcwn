d7680b3cad0498cdb8efe7426f888a0dff13e65e
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-02-25 00:12:49 +0000

    rebuild of evangellion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   output: 'static',
-  redirects: { '/posts/[...slug]': '/post/[...slug]' },
   site: 'https://mightydinosaur.dev/',
   vite: {
     plugins: [tailwindcss()],
